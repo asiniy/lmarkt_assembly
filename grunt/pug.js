25da3126777b273bd71e7befe4c76b3aa6499454
Michail Khlebnikov <frontend.dev.ru@gmail.com>
2020-07-23 20:43:55 +0300

    Fixes

--- grunt/pug.js
+++ grunt/pug.js
@@ -8,7 +8,11 @@ module.exports = {
     },
     files: {
       '<%=project.dist %>/index.html': '<%=project.app %>/tpls/index.pug',
-      '<%=project.dist %>/example.html': '<%=project.app %>/tpls/example.pug'
+      '<%=project.dist %>/sign-in-enter-name.html': '<%=project.app %>/tpls/sign-in-enter-name.pug',
+      '<%=project.dist %>/sign-in-enter-sms-invalid.html': '<%=project.app %>/tpls/sign-in-enter-sms-invalid.pug',
+      '<%=project.dist %>/sign-in-enter-sms.html': '<%=project.app %>/tpls/sign-in-enter-sms.pug',
+      '<%=project.dist %>/sign-in.html': '<%=project.app %>/tpls/sign-in.pug',
+      '<%=project.dist %>/list.html': '<%=project.app %>/tpls/list.pug'
     }
   }
 };
--- public/app-project.js
+++ public/app-project.js
@@ -1,13 +1,18 @@
-var app = new Vue({
-  el: '#app',
-  data: {},
-  computed: {},
-  methods: {
-    exampleMethod: function() {
-      console.log('exampleMethod init!');
-    }
-  },
-  mounted() {
-    this.exampleMethod;
+$(document).ready(function () {
+  // Sign in
+  function maskPhone () {
+    $.mask.definitions['~'] = '[9]';
+    $('.sign-in__form-input--phone').mask('+7 (~99) 999-99-99', {
+      autoclear: false
+    });
   }
+  maskPhone();
+
+  // Footer
+  function showHideFooterNav () {
+    $('.footer__nav-title-mobile').on('click', function () {
+      $(this).closest('.footer__nav-group-mobile').toggleClass('footer__nav-group-mobile--active');
+    });
+  }
+  showHideFooterNav();
 });
